9726968868c2fc412ce07d077a5d1103fdd8035f
Matheus Elias <matheusfernandes8694@gmail.com>
2024-10-03 16:23:29 -0300

    Criado o layout da página do cadastro de turma e também a página para mudar a senha caso esquecida.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,2 @@
-function registro(){ //Função para registrar o professor.
 
-    let inputSenha = document.getElementById('inputSenha').value
-    let inputValidar = document.getElementById('inputValidar').value
 
-    if(inputSenha == inputValidar){
-        alert('Registrado com Sucesso.')
-        document.getElementById('formRegistro').style.display  = "none"
-        document.getElementById('formLogin').style.display = "block"
-    }else{
-        alert('Senhas incorretas!')
-    }
-
-}
-
-//  function logar(){  
-//     http://127.0.0.1:5501/login.html
-// }
-
-// function cadastrarTurmas(){}
-
-// function cadastrarAtividades(){}
-
-//function mudarSenha(){}
